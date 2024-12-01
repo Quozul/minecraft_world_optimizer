65ca01aae6c3a5605699a3ce01b8168078cf2f5e
Quozul <30729291+Quozul@users.noreply.github.com>
2024-12-01 12:26:33 +0100

    feat: improve invalid directory error message

--- src/world/validate.rs
+++ src/world/validate.rs
@@ -3,14 +3,14 @@ use thiserror::Error;
 
 pub fn validate_worlds(worlds: &Vec<PathBuf>) -> Result<(), ValidateWorldError> {
     for world in worlds {
-        validate_world(&world)?;
+        validate_world(world)?;
     }
     Ok(())
 }
 
 fn validate_world(world_dir: &Path) -> Result<(), ValidateWorldError> {
     if !world_dir.is_dir() {
-        return Err(ValidateWorldError::NotADirectory);
+        return Err(ValidateWorldError::NotADirectory(world_dir.to_path_buf()));
     }
 
     if !std::fs::exists(world_dir.join("level.dat")).unwrap_or(false) {
@@ -22,8 +22,8 @@ fn validate_world(world_dir: &Path) -> Result<(), ValidateWorldError> {
 
 #[derive(Error, Debug)]
 pub enum ValidateWorldError {
-    #[error("the provided world directory is not a directory")]
-    NotADirectory,
+    #[error("the provided world `{0}` directory is not a directory")]
+    NotADirectory(PathBuf),
     #[error("the provided world directory is missing the `level.dat` file")]
     MissingLevelData,
 }
