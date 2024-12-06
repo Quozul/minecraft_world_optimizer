fab8017094634980de6096f2a4b850ec6bbbce49
Quozul <30729291+Quozul@users.noreply.github.com>
2024-12-06 17:22:25 +0100

    refactor: use macro for write array

--- src/nbt/writers.rs
+++ src/nbt/writers.rs
@@ -20,35 +20,23 @@ pub fn write_string(input: String) -> Vec<u8> {
     buffer
 }
 
-pub fn write_array_i8(input: &[i8]) -> Vec<u8> {
-    let mut buffer = create_initial_buffer::<i8>(input.len());
-    let bytes = input
-        .iter()
-        .flat_map(|e| e.to_be_bytes())
-        .collect::<Vec<u8>>();
-    buffer.extend(bytes);
-    buffer
+macro_rules! impl_write_array {
+    ($fn_name:ident, $type:ty) => {
+        pub fn $fn_name(input: &[$type]) -> Vec<u8> {
+            let mut buffer = create_initial_buffer::<$type>(input.len());
+            let bytes = input
+                .iter()
+                .flat_map(|e| e.to_be_bytes())
+                .collect::<Vec<u8>>();
+            buffer.extend(bytes);
+            buffer
+        }
+    };
 }
 
-pub fn write_array_i32(input: &[i32]) -> Vec<u8> {
-    let mut buffer = create_initial_buffer::<i32>(input.len());
-    let bytes = input
-        .iter()
-        .flat_map(|e| e.to_be_bytes())
-        .collect::<Vec<u8>>();
-    buffer.extend(bytes);
-    buffer
-}
-
-pub fn write_array_i64(input: &[i64]) -> Vec<u8> {
-    let mut buffer = create_initial_buffer::<i64>(input.len());
-    let bytes = input
-        .iter()
-        .flat_map(|e| e.to_be_bytes())
-        .collect::<Vec<u8>>();
-    buffer.extend(bytes);
-    buffer
-}
+impl_write_array!(write_array_i8, i8);
+impl_write_array!(write_array_i32, i32);
+impl_write_array!(write_array_i64, i64);
 
 #[cfg(test)]
 mod tests {
@@ -59,5 +47,35 @@ mod tests {
         let parsed = write_string("HELLO".to_string());
 
         assert_eq!(parsed, &[0, 5, 72, 69, 76, 76, 79]);
+    }
+
+    #[test]
+    fn test_write_array_i8() {
+        let parsed = write_array_i8(&[1, 2, 3, 4, 5]);
+
+        assert_eq!(parsed, &[0, 0, 0, 5, 1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_write_array_i32() {
+        let parsed = write_array_i32(&[1, 2, 3, 4, 5]);
+
+        assert_eq!(
+            parsed,
+            &[0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 5]
+        );
+    }
+
+    #[test]
+    fn test_write_array_i64() {
+        let parsed = write_array_i64(&[1, 2, 3, 4, 5]);
+
+        assert_eq!(
+            parsed,
+            &[
+                0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3,
+                0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5
+            ]
+        );
     }
 }
